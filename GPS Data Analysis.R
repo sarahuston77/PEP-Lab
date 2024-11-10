d3e2a6021c9031799f441cfcf030e49711b28c0d
Sara Huston <shuston007@gmail.com>
2024-11-10 08:00:01 -0500

    Update GPS Data Analysis.R

--- GPS Data Analysis.R
+++ GPS Data Analysis.R
@@ -1,114 +1,52 @@
 # Install packages to data wrangle
-install.packages("tidyverse")
 install.packages("data.table")
 
 # Load libraries
-library('tidyverse')
 library(data.table)
+library(dplyr)
 
 # Read data into a dataframe based on your path (can copy and paste this usally)
-df <- read.csv("/Users/sara/Downloads/GPS_filtered_fullmerge(in).csv")
+df <- read.csv("/Users/sara/Downloads/GPS_filtered_fullmerge.csv")
 
-# View the dataframe
-# View(df)
+# Round latitude and longitude coordinates to 4th decimal place
+df$lat_rounded <- round(df$Lat, 4)
+df$long_rounded <- round(df$Long, 4)
 
-# Add empty column
-df_with_new_col <- df %>%
-  add_column(home_cluster = NA)
-df_with_new_col
+# Merge latitude and longitude coordinates together
+df$lat_long_combined <- paste(sprintf("%.4f", df$lat_rounded), sprintf("%.4f", df$long_rounded), sep = "_")
 
-# View updated dataframe
-# View(df_with_new_col)
+# Create a column with only the hour, making it zero if only the date is present e.g., 12/7/18 (corresponds to 0 o'clock)
+df$hour <- suppressWarnings(as.numeric(format(as.POSIXct(df$Timestamp, format="%m/%d/%Y %H:%M"), "%H")))
+df$hour[is.na(df$hour)] <- 0
 
-# Create a df to keep track of the frequency of each time cluster
-scores_df <- data.frame(pid = character())
+# Filter the time so it between 12:00 AM and 6:00 AM and the latitude/longitude pair exists
+time_filtered <- df[df$hour >= 0 & df$hour < 6 & !is.na(df$hour) & !is.na(df$lat_long_combined) & df$lat_long_combined != "NA_NA", ]
 
-# Loop through each PID and create a df with
-# the frequency of each cluster between 1:00 AM and 6:00 AM
-row_index <- 1  # Initialize row index for accessing other columns
-for (timestamp in df_with_new_col$"Timestamp") {
+# Group by pid to find the most frequent value of latitude and longitude
+most_common <- time_filtered %>% group_by(pid) %>% summarize (lat_long_combined = names(which.max(table(lat_long_combined))))
+
+# Add home cluster to df based on pid matching
+df <- df %>% left_join(most_common, by = c("pid")) %>% rename(home_coordinate = lat_long_combined.y)
+
+# Renamed the column 
+colnames(df)[which(names(df) == "lat_long_combined.x")] <- "lat_long_combined"
+
+# Calculate total time per pid per date using grouping and adding 2 minutes for each match 
+total_time_per_pid_date <- df %>% group_by(Day, pid) %>% summarize(total_time_minutes = sum(ifelse(home_coordinate == lat_long_combined, 2, 0)), .groups = 'drop')
+
+# Calculating time spent at most common coordinate pair (each cell is associated with 2 minutes)
+df <- df %>% left_join(total_time_per_pid_date, by = c("Day", "pid"))
+
+# Percentage column
+df$percent_of_day <- df$total_time_minutes * 100 / 1440 
   
-  # Extract the hour
-  hour <- as.numeric(format(as.POSIXct(timestamp, format="%m/%d/%Y %H:%M"), "%H"))
-  
-  # Check if the time is between 1:00 AM and 6:00 AM
-  if (!is.na(hour) && hour >= 1 && hour < 6) {
-    
-    # pid in scores
-    pid_value <- df_with_new_col$pid[row_index]
-    timecluster <- as.character(df_with_new_col$timecluster[row_index])
-    
-    if (pid_value %in% scores_df[,"pid"]) {
-      # Position of pid in scores_df
-      position <- which(scores_df[,"pid"] == pid_value)
-      
-      # pid and time cluster in scores
-      if (timecluster %in% colnames(scores_df)) {
-        
-        # Add to the counter for the time_cluster
-        scores_df[position, timecluster] <- as.integer(scores_df[position, timecluster]) + 1
-      } else {
-      
-        # pid in scores, but not time_cluster
-        scores_df[, ncol(scores_df) + 1] <- 0
-        colnames(scores_df)[ncol(scores_df)] <- timecluster
-        scores_df[position, timecluster] <- 1
-      }
-      
-    } else {
-      
-      # Add pid to the scores df since it is not there
-      new_row <- as.list(rep(0, ncol(scores_df)))
-      names(new_row) <- colnames(scores_df)
-      new_row$pid <- pid_value
-      scores_df <- rbind(scores_df, new_row)
-      
-      # Position of pid in scores_df
-      position <- which(scores_df[,"pid"] == pid_value)
-      
-      # pid and time cluster in scores
-      if (timecluster %in% colnames(scores_df)) {
-        
-        # Add to the counter for the time_cluster
-        scores_df[position, timecluster] <- scores_df[position, timecluster] + 1
-        
-      } else {
-        # pid in scores, but not time_cluster
-        scores_df[, ncol(scores_df) + 1] <- 0
-        colnames(scores_df)[ncol(scores_df)] <- timecluster
-        scores_df[position, timecluster] <- 1
-      }
-    }
-  }
-  
-  # Increment the row index
-  row_index <- row_index + 1
-}
+# View final df
+View(df)
 
-# See the most common places
-View(scores_df)
-
-# Map pids to max columns
-row_index_2 <- 1
-pid_values <- scores_df$pid
-pid_max_col_map <- sapply(1:nrow(scores_df), function(row_index) {
-  colnames(scores_df)[which.max(scores_df[row_index, ])]
-})
-names(pid_max_col_map) <- pid_values
-
-# Match the pid's across dataframes
-match_indices <- match(df_with_new_col$pid, pid_values)
-max_cols <- pid_max_col_map[match_indices]
-
-# Add the new col with the home data
-df_with_new_col[, ncol(df_with_new_col)] <- max_cols
-
-View(df_with_new_col)
-
+# Make sure working directly is set correctly for output
 getwd()
 setwd("/Users/sara/Downloads")
 
 # Save the data frame as the CSV file
-write.csv(df_with_new_col, "GPS_filtered_fullmerge(in).csv")
-
-print("CSV file has been written successfully.")
+write.csv(df, "GPS_filtered_fullmerge.csv")
+print("CSV outputted.")
